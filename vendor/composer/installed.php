b557b49bd02401d9d945d87327a548fedd7a791b
MuhammadAthalla <juraganathalla@gmail.com>
2023-02-08 08:39:55 +0700

    change name

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'pph21/calculator',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'name' => 'pajak/pph21',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '61e9e410a587bba66e977f1cfdd62db1bfc4dbd5',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'pph21/calculator' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+        'pajak/pph21' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '61e9e410a587bba66e977f1cfdd62db1bfc4dbd5',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
